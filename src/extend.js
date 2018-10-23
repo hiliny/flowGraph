5e0daa4f41355dbbe05d6cb202ff00397924f7f5
hiliny <1242043471@qq.com>
2018-10-23 19:43:00 +0800

    some modify

---
+++ src/extend.js
@@ -0,0 +1,54 @@
+function drawArrow(ctx, fromX, fromY, toX, toY, theta, headlen, width, color) {
+    // theta夹角,headlen三角斜边长度
+    theta = typeof (theta) != 'undefined' ? theta : 30;
+    headlen = typeof (theta) != 'undefined' ? headlen : 10;
+    width = typeof (width) != 'undefined' ? width : 1;
+    color = typeof (color) != 'color' ? color : '#000';
+    var angle = Math.atan2(fromY - toY, fromX - toX) * 180 / Math.PI,
+        angle1 = (angle + theta) * Math.PI / 180,
+        angle2 = (angle - theta) * Math.PI / 180,
+        topX = headlen * Math.cos(angle1),
+        topY = headlen * Math.sin(angle1),
+        botX = headlen * Math.cos(angle2),
+        botY = headlen * Math.sin(angle2);
+    ctx.save();
+    ctx.beginPath();
+    var arrowX = fromX - topX,
+        arrowY = fromY - topY;
+    ctx.moveTo(arrowX, arrowY);
+    ctx.moveTo(fromX, fromY);
+    ctx.lineTo(toX, toY);
+    arrowX = toX + topX;
+    arrowY = toY + topY;
+    ctx.moveTo(arrowX, arrowY);
+    ctx.lineTo(toX, toY);
+    arrowX = toX + botX;
+    arrowY = toY + botY;
+    ctx.lineTo(arrowX, arrowY);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width;
+    ctx.stroke();
+    ctx.restore();
+}
+
+function test(fromX, fromY, toX, toY, theta, headlen){
+    theta = typeof (theta) != 'undefined' ? theta : 30;
+    headlen = typeof (theta) != 'undefined' ? headlen : 10;
+    var angle = Math.atan2(fromY - toY, fromX - toX) * 180 / Math.PI,
+        angle1 = (angle + theta) * Math.PI / 180,
+        angle2 = (angle - theta) * Math.PI / 180,
+        topX = headlen * Math.cos(angle1),
+        topY = headlen * Math.sin(angle1),
+        botX = headlen * Math.cos(angle2),
+        botY = headlen * Math.sin(angle2);
+
+    var pot = {
+        x1:fromX-topX,
+        y1:fromY-topY,
+        x2:toX+botX,
+        y2:toY+botY
+    }; 
+        
+    console.log(pot);
+}
+test(4,5,30,40,45,10);
--- src/lib.js
+++ src/lib.js
@@ -6,69 +6,83 @@ var SHAPE_RECT_HEIGHT = 40; //流程节点的高度
 var LINE_WIDTH = 2; //流程节点的框线宽度
 var nodeCache = []; //流程节点的全局缓存
 var lineCache = []; //全局缓存连接线对象
-var currentNode = null;//当前的选中节点
-var checkedNode = false;//是否点击选中了节点
-var isAddingLine = false;//是否正在添加连接线
+var currentNode = null; //当前的选中节点
+var checkedNode = false; //是否点击选中了节点
+var isAddingLine = false; //是否正在添加连接线
 var linePosition = []; //划线的起止点
 
 // 主线流程
 var divContainer = document.getElementById("main");
-var zr = zrender.init(divContainer,null,{renderer:'canvas',devicePixelRatio:2});
-var domNode = document.getElementById("addNode"),domLine = document.getElementById("addLine"),
+var zr = zrender.init(divContainer, null, {
+    renderer: 'canvas',
+    devicePixelRatio: 2
+});
+var domNode = document.getElementById("addNode"),
+    domLine = document.getElementById("addLine"),
     delNode = document.getElementById("delNode");
+console.log(zrender.Element);
 
 // 点击canvas容器，取消激活节点
-zr.on('click',function(){
-    if(!currentNode) return;
-    if(checkedNode) return checkedNode = false;
-    currentNode.attr({style:{stroke:SHAPE_LINE_COLOR}});
+zr.on('click', function () {
+    if (!currentNode) return;
+    if (checkedNode) return checkedNode = false;
+    currentNode.attr({
+        style: {
+            stroke: SHAPE_LINE_COLOR
+        }
+    });
     currentNode = null;
 });
-zr.on('mousedown',function(e){
-    if(isAddingLine){
+zr.on('mousedown', function (e) {
+    if (isAddingLine) {
         linePosition.push({
-            x:e.offsetX,
-            y:e.offsetY
+            x: e.offsetX,
+            y: e.offsetY
         });
     }
 });
-zr.on('mouseup',function(e){
-    if(!isAddingLine) return;
+zr.on('mouseup', function (e) {
+    if (!isAddingLine) return;
     linePosition.push({
-        x:e.offsetX,
-        y:e.offsetY
+        x: e.offsetX,
+        y: e.offsetY
     });
-    var line = createLine.apply(this,linePosition);
+    var line = createLine.apply(this, linePosition);
     zr.add(line);
     linePosition.length = 0;
     isAddingLine = false;
-    disableDraggable(nodeCache,true);
+    disableDraggable(nodeCache, true);
 });
 /**
  * 辅助函数
  */
 // 添加节点事件
-domNode.addEventListener('click',function(){
-    var txt = document.getElementById('nodeTxt').value,shape = createShape(txt);
+domNode.addEventListener('click', function () {
+    var txt = document.getElementById('nodeTxt').value,
+        shape = createShape(txt);
     nodeCache.push(shape);
-    shape.on('click',function(ev){
-        this.attr({style:{stroke:ACTIVE_COLOR}});
+    shape.on('click', function (ev) {
+        this.attr({
+            style: {
+                stroke: ACTIVE_COLOR
+            }
+        });
         currentNode = this;
         checkedNode = true;
     });
     zr.add(shape);
 });
 //添加连接线事件
-domLine.addEventListener('click',function(){
+domLine.addEventListener('click', function () {
     isAddingLine = true;
-    disableDraggable(nodeCache,false);
+    disableDraggable(nodeCache, false);
 });
 //删除节点
-delNode.addEventListener('click',function(){
+delNode.addEventListener('click', function () {
     deleteNode();
 });
 // 创建流程节点函数
-function createShape(nodeName){
+function createShape(nodeName) {
     var shape = new zrender.Rect({
         shape: {
             x: 50,
@@ -80,49 +94,59 @@ function createShape(nodeName){
             fill: SHAPE_COLOR,
             stroke: SHAPE_LINE_COLOR,
             lineWidth: LINE_WIDTH,
-            truncate:{
-                outerWidth:100
+            truncate: {
+                outerWidth: 100
             },
-            text:nodeName || '新节点'
+            text: nodeName || '新节点'
         },
-        draggable:true
+        draggable: true
     });
     return shape;
 }
 //创建线性节点
-function createLine(a,b){
-    a = a || {x:10,y:10};
-    b = b || {x:60,y:60};
+function createLine(a, b) {
+    a = a || {
+        x: 10,
+        y: 10
+    };
+    b = b || {
+        x: 60,
+        y: 60
+    };
     var line = new zrender.Line({
-        shape:{
-            x1:a.x,
-            y1:a.y,
-            x2:b.x,
-            y2:b.y
+        shape: {
+            x1: a.x,
+            y1: a.y,
+            x2: b.x,
+            y2: b.y
         },
-        style:{
-            stroke:SHAPE_LINE_COLOR,
-            lineWidth:LINE_WIDTH,
-            lineCap:'butt'
+        style: {
+            stroke: SHAPE_LINE_COLOR,
+            lineWidth: LINE_WIDTH,
+            lineCap: 'butt'
         },
-        draggable:true
+        draggable: true
     });
     nodeCache.push(line);
-    line.on('click',function(ev){
-        this.attr({style:{stroke:ACTIVE_COLOR}});
+    line.on('click', function (ev) {
+        this.attr({
+            style: {
+                stroke: ACTIVE_COLOR
+            }
+        });
         currentNode = this;
         checkedNode = true;
     });
     zr.add(line);
-    return line;   
+    return line;
 }
 //删除选中节点
-function deleteNode(){
+function deleteNode() {
     var len = nodeCache.length - 1;
-    if(!currentNode) return;
-    for(var i = len;i>=0;i--){
-        if(nodeCache[i] == currentNode){
-            nodeCache.splice(i,1);
+    if (!currentNode) return;
+    for (var i = len; i >= 0; i--) {
+        if (nodeCache[i] == currentNode) {
+            nodeCache.splice(i, 1);
             currentNode.off('click');
             zr.remove(currentNode);
             currentNode = null;
@@ -131,12 +155,10 @@ function deleteNode(){
     }
 }
 // 禁用图形节点的拖动功能
-function disableDraggable(nodes,useAble){
-    nodes.forEach(function(node){
+function disableDraggable(nodes, useAble) {
+    nodes.forEach(function (node) {
         node.attr({
-            draggable:useAble
+            draggable: useAble
         });
     });
 }
-
-
